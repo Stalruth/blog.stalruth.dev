d3a90fb600b8fe560a706ce66cd68e96fc5924f0
Stuart Allan Rutherford <stalruth@gmail.com>
2022-11-05 02:03:06 +1100

    add _headers

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,6 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ 'node_modules/mvp.css/mvp.css' : 'css/mvp.css' });
-  eleventyConfig.addPassthroughCopy({ 'css' : 'css' });
+  eleventyConfig.addPassthroughCopy({ 'static' : '/' });
   return {
     "dir": {
       "input": "content",
