5ee1708f83da0dbd9e0a0744afd2f7f715385d4f
Stuart Allan Rutherford <stalruth@gmail.com>
2022-11-10 18:39:04 +1100

    Add RSS feed

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,8 @@
+const pluginRss = require("@11ty/eleventy-plugin-rss");
+
 module.exports = function(eleventyConfig) {
+  eleventyConfig.addLiquidFilter("getNewestCollectionItemDate", pluginRss.getNewestCollectionItemDate);
+  eleventyConfig.addLiquidFilter("dateToRfc822", pluginRss.dateToRfc822);
   eleventyConfig.addPassthroughCopy({ 'node_modules/mvp.css/mvp.css' : 'css/mvp.css' });
   eleventyConfig.addPassthroughCopy({ 'static' : '/' });
   return {
